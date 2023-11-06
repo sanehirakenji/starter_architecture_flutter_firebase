5344ab18492e19ecfb77404d5953674f2f52f205
Kenji Sanehira <kenjisan@hawaii.edu>
2023-11-05 23:30:54 -1000

    Used Freezed for Entry and Job data model classes.

--- lib/src/features/entries/domain/entry.dart
+++ lib/src/features/entries/domain/entry.dart
@@ -1,27 +1,21 @@
-import 'package:equatable/equatable.dart';
-import 'package:starter_architecture_flutter_firebase/src/features/jobs/domain/job.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+import '../../jobs/domain/job.dart';
+
+part 'entry.freezed.dart';
+part 'entry.g.dart';
 
 typedef EntryID = String;
 
-class Entry extends Equatable {
-  const Entry({
-    required this.id,
-    required this.jobId,
-    required this.start,
-    required this.end,
-    required this.comment,
-  });
-  final EntryID id;
-  final JobID jobId;
-  final DateTime start;
-  final DateTime end;
-  final String comment;
-
-  @override
-  List<Object> get props => [id, jobId, start, end, comment];
-
-  @override
-  bool get stringify => true;
+@Freezed()
+class Entry with _$Entry {
+  const Entry._();
+  const factory Entry({
+    required String id,
+    required JobID jobId,
+    required DateTime start,
+    required DateTime end,
+    required String comment,
+  }) = _Entry;
 
   double get durationInHours =>
       end.difference(start).inMinutes.toDouble() / 60.0;
@@ -46,4 +40,7 @@ class Entry extends Equatable {
       'comment': comment,
     };
   }
+
+  factory Entry.fromJson(Map<String, dynamic> json) => _$EntryFromJson(json);
+
 }
